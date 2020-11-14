594e6241fe0651c46f385d3b172518ce99cdd4c1
Liviu Strati <liviu.strati@hdt.ro>
2020-11-14 12:47:23 +0200

    prima varianta functionala

--- app.php
+++ app.php
@@ -1,3 +1,30 @@
 <?php
 echo "buna dimineata";
+echo "<br>";
+echo "versiune noua 3";
+
+$servername = "192.168.10.66";
+$username = "iot_user";
+$password = "init1234";
+$dbname = "iot";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM devices";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["iddevices"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
